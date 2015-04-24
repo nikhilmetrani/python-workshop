a47296760ed082a031e0f2173e995ce558fb85de
Nikhil Metrani <nikhilmetrani@gmail.com>
2015-04-24 22:33:11 +0800

    Conditional statements - nested and chained Range and String operators - Concatnation and Multiplicity

--- workshop1/conditional.py
+++ workshop1/conditional.py
@@ -4,12 +4,28 @@ import sys
 
 n = 10
 
+#Conditional statement
 if len(sys.argv) >= 2:
     n = int(sys.argv[1]) #type cast to int is necessary otherwise the argument will be treated as string
 
+#Chained conditional
 if n > 0:
     print n, "is positive"
 elif n < 0:
     print n, "is negative"
 else:
     print n, "is neutral"
+
+#Nested conditional
+if len(sys.argv) == 3:
+    p = int(sys.argv[2])
+    if n > p:
+        print "Before: n=", n, " p=", p
+        n, p = p, n
+        print "n, p = p, n: n=", n, " p=", p
+    else:
+        if n==p:
+            p = p + 1
+        else:
+            print "recorder not necessary"
+
---
+++ workshop1/stringOps.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+a="abc"
+b="xyz"
+print "Concatnation a+b:", a + b
+print "Multiplicity a*3:", a*3
+print "Multiplicity b*3:", b*3
+
+print "Next line: print \"Multiplicity a*b:\", a*b, will result into an error"
+print "Multiplicity a*b:", a*b
